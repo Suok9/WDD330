0308041bba31b97011ca7b9d53101c1c3585e711
ivan-pl809 <pon20007@byui.edu>
2025-10-07 20:47:47 -0600

    activity completed

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,22 @@
-import {getLocalStorage, setLocalStorage} from './utils.mjs';
+import { setLocalStorage, getLocalStorage, alertMessage } from "./utils.mjs";
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Images.PrimaryLarge}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -7,37 +25,32 @@ export default class ProductDetails {
     this.dataSource = dataSource;
   }
   async init() {
-    // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
-    console.log('Product loaded:', this.product);
-    // the product details are needed before rendering the HTML
-    this.render();
-    // once the HTML is rendered, add a listener to the Add to Cart button
-    // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
-    document.getElementById('addToCart')
-        .addEventListener('click', this.addProductToCart.bind(this));
+    // once we have the product details we can render out the HTML
+    this.renderProductDetails("main");
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
   }
-
-    addProductToCart(product) {
-      let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
-      cart.push(this.product);
-      localStorage.setItem('so-cart', JSON.stringify(cart));
+  addToCart() {
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+      cartContents = [];
     }
-
-    render() {
-      this.renderProductDetails(this.product);
-    }
-     renderProductDetails(product) {
-        document.getElementsByClassName('product-card__price')[0].textContent = product.FinalPrice;
-        document.getElementsByClassName('product__color')[0].textContent = product.Colors[0].ColorName;
-        document.getElementsByClassName('product__description')[0].innerHTML = product.DescriptionHtmlSimple;
-        document.querySelector('h3').textContent = product.Brand.Name;
-        document.querySelector('h2').textContent = product.Name;
-
-        const productImage = document.getElementById('productImage');
-        productImage.src = product.Image
-        productImage.alt = product.Name;
-
-        
-    }
+    // then add the current product to the list
+    cartContents.push(this.product);
+    setLocalStorage("so-cart", cartContents);
+    alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
+  }
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+      "afterBegin",
+      productDetailsTemplate(this.product)
+    );
+  }
 }
